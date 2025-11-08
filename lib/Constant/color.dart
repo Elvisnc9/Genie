75b9a632fa05eb6ded4d0ae27950665152012bd3
elvisnc <148381430+Elvisnc9@users.noreply.github.com>
2025-11-08 17:01:22 +0100

    Structure

---
+++ lib/Constant/color.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color dark = Color(0xFF040E00);
+  static const Color light = Color(0XFFFEAF2EF);
+  static const Color berry = Color(0XFFF912F56);
+  static const Color golden = Color(0XFFFF8C828);
+  
+
+static const LinearGradient minty = LinearGradient(colors: [
+    Color(0XFFF97A87A),
+     Color(0XFFFDAD7CD),
+  ],
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight
+  );
+ 
+}
+
+class Spref {
+  static const String isLight = "isLight";
+}
